45aa2062898293e97b6090c3972d798b72724e15
mundoIndustrial <mundoindustrialsoftware@gmail.com>
2025-11-11 15:01:38 -0500

    cambios del filtro pedido

--- app/Http/Controllers/RegistroBodegaController.php
+++ app/Http/Controllers/RegistroBodegaController.php
@@ -90,7 +90,7 @@ class RegistroBodegaController extends Controller
 
                 // Whitelist de columnas permitidas para seguridad
                 $allowedColumns = [
-                    'id', 'estado', 'area', 'total_de_dias_', '_pedido', 'cliente',
+                    'id', 'estado', 'area', 'total_de_dias_', 'pedido', 'cliente',
                     'descripcion', 'cantidad', 'novedades', 'asesora', 'forma_de_pago',
                     'fecha_de_creacion_de_orden', 'encargado_orden', 'dias_orden', 'inventario',
                     'encargados_inventario', 'dias_inventario', 'insumos_y_telas', 'encargados_insumos',
