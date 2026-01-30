d48b5d7df4018d55429345f23381e130ee551c8f
Mindustrial <mindustrial@mundoindustrial.com>
2026-01-30 11:57:33 -0500

    Merge branch 'main' of https://github.com/mundoIndustrial/software-gestion

--- vite-remote-config.js
+++ vite-remote-config.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const ip = '192.168.0.172';
+=======
+const ip = '192.168.0.245';
+>>>>>>> dce8a74e69c9150a2ee0f7334f60df1f9fce8c93
 export default {
   viteHmrHost: ip,
   viteHmrPort: 5173,
